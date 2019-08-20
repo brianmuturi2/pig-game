eab3d02c5c78b1c79c4e8718d09a16dd12fa0865
Brian Muturi <brianmuturi2@gmail.com>
2019-08-20 11:28:08 +0300

    Implemented game initialization functionality

--- app.js
+++ app.js
@@ -11,16 +11,32 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-document.querySelector('.dice').style.display = 'none';
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+
+};
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     var dice, diceDom;
@@ -78,6 +94,7 @@ function togglePlayer(){
     document.querySelector('.dice').style.display = 'none';
 };
 
+document.querySelector('.btn-new').addEventListener('click', init);
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
