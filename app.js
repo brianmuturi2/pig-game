d2b723b8df31d5d3e993d79d41f0ed9018fa9223
Brian Muturi <brianmuturi2@gmail.com>
2019-08-20 08:31:18 +0300

    Event handling rolling the dice: replace dice image according to dice rolled (random no generated) & resseting total  scores and current scores

--- app.js
+++ app.js
@@ -9,22 +9,53 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6) + 1;
-console.log(dice);
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var dice, diceDom;
+    diceDom = document.querySelector('.dice');
+
+    // 1. random no.
+    dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. display result
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    // 3. update round score if the number isnt 1
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
 
 
